a8a972a0d65666051fb530937d279f0448293bfc
Zanele Moni <zanelemoni4@gmail.com>
2022-03-19 11:55:50 +0200

    deleted blog

--- routes/blogs.Routes.js
+++ routes/blogs.Routes.js
@@ -6,7 +6,7 @@ const blogModels = require("../models/blog");
 router.get("/", async (req, res) => {
   try {
     const blogs = await blogModels.find();
-    res.json(blogs);
+    res.status(200).json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
