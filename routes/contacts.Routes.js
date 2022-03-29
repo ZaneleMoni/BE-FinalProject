ec05e33d391735346b058227a9c85523aee43e4e
Zanele Moni <zanelemoni4@gmail.com>
2022-03-29 15:12:17 +0200

    contact

--- routes/contacts.Routes.js
+++ routes/contacts.Routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
-
 const router = express.Router();
+require("dotenv").config();
 
 router.get('/', (req, res) => {
   res.send({ msg: "getting contacts" });
@@ -14,11 +13,11 @@ router.post("/", (req, res) => {
   console.log(email, name, subject, message);
   console.log(req.body)
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
+  const transporter = nodemailer.createTransport({
+    // service: "gmail",
     host: "smtp.gmail.com",
     port: 465,
-    secure: true,
+    // secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASS,
@@ -26,7 +25,7 @@ router.post("/", (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
+    from: `${email}`,
     to: "zanelemoni4@gmail.com",
     subject: `${subject}`,
     text: `${name} has contacted you please contact them back on ${email} ${message}`,
@@ -36,7 +35,7 @@ router.post("/", (req, res) => {
     if (err) {
       console.log(err);
       res.status(400).send({ msg: "email not sent" });
-    } else {
+    } else {cont-
       console.log("Email sent: " + info.response);
       res.send({ msg: "email has been sent successfully" });
     }
